a6faf1ec5dc81654557dfa6b34f6b0b5ed0a51e0
jbolivar <j_bolivar@outlook.com>
2021-02-12 17:44:04 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ sudo apt-get update -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3  get-pip.py
 python3 -m pip install ansible
-ansible-galaxy install -r requirements.txt -p .
+ansible-galaxy install -r requirements.yml -p .
 ansible-playbook k8s.yml
 
 
