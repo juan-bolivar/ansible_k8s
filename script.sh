a02b19ca87cba432e0c7124feea32016005c9795
jbolivar <j_bolivar@outlook.com>
2021-02-12 17:29:55 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3  get-pip.py
 python3 -m pip install ansible
-ansible-galaxy install -r requirements.txt -p 
+ansible-galaxy install -r requirements.txt -p .
 ansible-playbook k8s.yml
 
 
