bfeea9f6f02a35e0481676f50ee9f7d90634f566
Juan Bolivar <juan@jbolivar.co>
2021-02-12 17:19:19 -0500

    Added k8s.yml

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 -m get-pip.py
 python3 -m pip install ansible
-
+ansible-galaxy install -r requirements.txt -p 
+ansible-playbook k8s.yml.
 
 
 
